fd81e661d2b8b37686f65cc379b809e5de4c90de
Trezy <tre@trezy.com>
2021-03-03 10:33:24 -0600

    give validation endpoints more appropriate names

--- packages/web/src/pages/api/users/validate-email.js
+++ packages/web/src/pages/api/users/validate-email.js
@@ -11,20 +11,16 @@ import httpStatus from 'helpers/httpStatus'
 
 
 export const handler = async (request, response) => {
-	const { username } = request.query
+	const { email } = request.query
 
 	try {
-		const users = await firestore
-			.collection('profiles')
-			.where('username', '==', username)
-			.get()
-
-		if (users.size) {
-			return response.status(httpStatus.CONFLICT).end()
+		await auth.getUserByEmail(email)
+		return response.status(httpStatus.FOUND).end()
+	} catch (error) {
+		if (error.errorInfo.code === 'auth/user-not-found') {
+			return response.status(httpStatus.NO_CONTENT).end()
 		}
 
-		return response.status(httpStatus.NO_CONTENT).end()
-	} catch (error) {
 		console.log(error)
 		return response.status(httpStatus.INTERNAL_SERVER_ERROR).end()
 	}
--- packages/web/src/pages/api/users/validate-username.js
+++ packages/web/src/pages/api/users/validate-username.js
@@ -11,16 +11,20 @@ import httpStatus from 'helpers/httpStatus'
 
 
 export const handler = async (request, response) => {
-	const { email } = request.query
+	const { username } = request.query
 
 	try {
-		await auth.getUserByEmail(email)
-		return response.status(httpStatus.CONFLICT).end()
-	} catch (error) {
-		if (error.errorInfo.code === 'auth/user-not-found') {
-			return response.status(httpStatus.NO_CONTENT).end()
+		const users = await firestore
+			.collection('profiles')
+			.where('username', '==', username)
+			.get()
+
+		if (users.size) {
+			return response.status(httpStatus.FOUND).end()
 		}
 
+		return response.status(httpStatus.NO_CONTENT).end()
+	} catch (error) {
 		console.log(error)
 		return response.status(httpStatus.INTERNAL_SERVER_ERROR).end()
 	}
