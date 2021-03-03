2c536327629eabec30305e1d9257aa11e4a97cbb
Trezy <tre@trezy.com>
2021-03-03 09:51:06 -0600

    add user field verification endpoints

---
+++ packages/web/src/pages/api/users/check-username.js
@@ -0,0 +1,40 @@
+// Local imports
+import {
+	auth,
+	firestore,
+} from 'helpers/firebase.admin'
+import { createEndpoint } from 'helpers/createEndpoint'
+import httpStatus from 'helpers/httpStatus'
+
+
+
+
+
+export const handler = async (request, response) => {
+	const { username } = request.query
+
+	try {
+		const users = await firestore
+			.collection('profiles')
+			.where('username', '==', username)
+			.get()
+
+		if (users.size) {
+			return response.status(httpStatus.CONFLICT).end()
+		}
+
+		return response.status(httpStatus.NO_CONTENT).end()
+	} catch (error) {
+		console.log(error)
+		return response.status(httpStatus.INTERNAL_SERVER_ERROR).end()
+	}
+}
+
+
+
+
+
+export default createEndpoint({
+	allowedMethods: ['get'],
+	handler,
+})
