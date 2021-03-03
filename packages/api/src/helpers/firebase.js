18751310e240a6e272076691cf2bf1b80bb069c6
Trezy <tre@trezy.com>
2021-03-03 00:19:04 -0600

    add support for NEXT_PUBLIC_CURRENT_DEV

--- packages/api/src/helpers/firebase.js
+++ packages/api/src/helpers/firebase.js
@@ -42,6 +42,12 @@ if (!auth) {
 
 if (!firestore) {
 	firestore = app?.firestore()
+
+	if (process.env.NEXT_PUBLIC_CURRENT_DEV) {
+		firestore = firestore
+			.collection('dev')
+			.doc(process.env.NEXT_PUBLIC_CURRENT_DEV)
+	}
 }
 
 
