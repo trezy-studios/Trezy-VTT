663da1e85841ef07582bbca1824da1e88f360b9a
Trezy <tre@trezy.com>
2021-03-07 22:51:37 -0600

    add grid rendering to VTT

--- packages/vtt/src/helpers/scaleCanvas.js
+++ packages/vtt/src/helpers/scaleCanvas.js
@@ -1,9 +1,14 @@
-export function scaleCanvas(canvasElement) {
-	const { parentNode } = canvasElement
+export function scaleCanvas(primaryCanvasElement, ...otherCanvasElements) {
+	const { parentNode } = primaryCanvasElement
 
+	const allCanvasElements = [primaryCanvasElement, ...otherCanvasElements]
 	const dpr = window.devicePixelRatio || 1
+	const height = parentNode.clientHeight
+	const width = parentNode.clientWidth
 
-	canvasElement.getContext('2d').scale(dpr, dpr)
-	canvasElement.height = parentNode.clientHeight
-	canvasElement.width = parentNode.clientWidth
+	allCanvasElements.forEach(canvasElement => {
+		canvasElement.getContext('2d').scale(dpr, dpr)
+		canvasElement.height = height
+		canvasElement.width = width
+	})
 }
