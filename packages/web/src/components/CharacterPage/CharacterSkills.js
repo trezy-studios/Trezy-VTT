ccae7535f78dccb4332ab7da44fd561488fba88e
Trezy <tre@trezy.com>
2021-03-17 22:46:29 -0500

    pull skill values from character sheet

--- packages/web/src/components/CharacterPage/CharacterSkills.js
+++ packages/web/src/components/CharacterPage/CharacterSkills.js
@@ -7,36 +7,8 @@ import PropTypes from 'prop-types'
 
 
 
-// Local imports
-import { calculateAbilityModifier } from 'helpers/calculateAbilityModifier'
-import { JSONPreview } from 'components/JSONPreview'
-
-
-
-
-
 // Constants
-const SKILL_ABILITY = {
-	acrobatics: 'dexterity',
-	animalHandling: 'wisdom',
-	arcana: 'intelligence',
-	athletics: 'strength',
-	deception: 'charisma',
-	history: 'intelligence',
-	insight: 'wisdom',
-	intimidation: 'charisma',
-	investigation: 'intelligence',
-	medicine: 'wisdom',
-	nature: 'intelligence',
-	perception: 'wisdom',
-	performance: 'charisma',
-	persuasion: 'charisma',
-	religion: 'intelligence',
-	sleightOfHand: 'dexterity',
-	stealth: 'dexterity',
-	survival: 'wisdom',
-}
-const SKILL_DISPLAY_NAME = {
+const SKILLS = {
 	acrobatics: 'Acrobatics',
 	animalHandling: 'Animal Handling',
 	arcana: 'Arcana',
@@ -62,49 +34,18 @@ const SKILL_DISPLAY_NAME = {
 
 
 function CharacterSkills(props) {
-	const {
-		proficiencyBonus,
-		skills,
-		stats,
-	} = props.character
+	const { characterSheet } = props.character
 
-	function calculateSkillValue(skill, details, proficiencyBonus) {
-		let value = calculateAbilityModifier(stats[SKILL_ABILITY[skill]])
-
-		if (details.proficient) {
-			value += proficiencyBonus
-		}
-
-		if (details.expert) {
-			value *= 2
-		}
-
-		return value
-	}
-
-	const mapSkill = useCallback(([skill, details]) => {
-		let skillScore = calculateAbilityModifier(stats[SKILL_ABILITY[skill]])
-
-		if (details.proficient) {
-			skillScore += proficiencyBonus
-		}
-
-		if (details.expert) {
-			skillScore *= 2
-		}
-
+	const mapSkill = useCallback(([skill, skillDisplayName]) => {
 		return (
 			<tr key={skill}>
-				<th>{SKILL_DISPLAY_NAME[skill]}</th>
+				<th>{skillDisplayName}</th>
 				<td className="has-text-right">
-					{skillScore}
+					{characterSheet[skill]}
 				</td>
 			</tr>
 		)
-	}, [
-		stats,
-		proficiencyBonus,
-	])
+	}, [characterSheet])
 
 	return (
 		<details
@@ -124,7 +65,7 @@ function CharacterSkills(props) {
 
 			<table className="card-content is-fullwidth is-hoverable is-striped table">
 				<tbody>
-					{Object.entries(skills).map(mapSkill)}
+					{Object.entries(SKILLS).map(mapSkill)}
 				</tbody>
 			</table>
 		</details>
