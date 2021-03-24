9524f0db8268ceb41f8ecc35501c17f181df2e89
Trezy <tre@trezy.com>
2021-03-24 16:35:34 -0500

    move reward creation into campaigns context

--- packages/web/src/pages/api/rewards/new.js
+++ packages/web/src/pages/api/rewards/new.js
@@ -18,25 +18,24 @@ export const handler = async (request, response) => {
 	try {
 		const user = await auth.verifyIdToken(firebaseAuthToken, true)
 
-		if (!reward.title || !reward.cost || reward.uid !== user.uid) {
-			response.status(httpStatus.UNPROCESSABLE_ENTITY).end()
+		if (!reward.title || !reward.cost) {
+			return response.status(httpStatus.UNPROCESSABLE_ENTITY).end()
 		}
-        
-		const rewardsCollection = firestore
-                                    .collection('campaigns')
-                                    .doc(reward.campaignID)
-                                    .collection('rewards')
 
-        delete reward.campaignID
-        delete reward.uid
+		const campaignRef = firestore.collection('campaigns').doc(reward.campaignID)
+		const campaignDoc = await campaignRef.get()
+		const campaign = campaignDoc.data()
+
+		if (campaign.ownerID !== user.uid) {
+			return response.status(httpStatus.FORBIDDEN).end()
+		}
+
 		reward.createdAt = firebase.firestore.FieldValue.serverTimestamp()
-	    reward.updatedAt = firebase.firestore.FieldValue.serverTimestamp()
+		reward.updatedAt = firebase.firestore.FieldValue.serverTimestamp()
 
-		const newReward = await rewardsCollection.add(reward)
+		await campaignRef.collection('rewards').add(reward)
 
-		response.status(httpStatus.PARTIAL_CONTENT).json({
-			id: newReward.id,
-		})
+		response.status(httpStatus.OK).end()
 	} catch (error) {
 		console.log(error)
 
