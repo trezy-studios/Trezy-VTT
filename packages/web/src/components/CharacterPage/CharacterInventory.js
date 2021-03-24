e795d6eb598b7395f2c1093980b3c4bf068b7b00
Trezy <tre@trezy.com>
2021-03-23 20:56:55 -0500

    move temp inventory into the inventory component

--- packages/web/src/components/CharacterPage/CharacterInventory.js
+++ packages/web/src/components/CharacterPage/CharacterInventory.js
@@ -11,6 +11,99 @@ import ReactMarkdown from 'react-markdown'
 import { Button } from 'components/Button'
 import { calculateAbilityModifier } from 'helpers/calculateAbilityModifier'
 import { FontAwesomeIcon } from 'components/FontAwesomeIcon'
+import { useCharacters } from 'contexts/CharactersContext'
+
+
+
+
+
+// Constants
+// TODO: This should exist as dynamic values in the database
+const TEMP_INVENTORY = [
+	{
+		equippable: true,
+		imageURL: '',
+		isAttuned: false,
+		isEquipped: false,
+		name: 'Battleaxe, +3',
+		quantity: 1,
+		rarity: 3,
+		requiresAttunement: false,
+		weight: 4,
+	},
+
+	{
+		equippable: true,
+		imageURL: '',
+		isAttuned: true,
+		isEquipped: true,
+		name: 'Spellguard Shield',
+		quantity: 1,
+		rarity: 3,
+		requiresAttunement: true,
+		weight: 6,
+	},
+
+	{
+		equippable: true,
+		imageURL: '',
+		isAttuned: true,
+		isEquipped: true,
+		name: 'Cloak of Displacement',
+		quantity: 1,
+		rarity: 2,
+		requiresAttunement: true,
+		weight: 0,
+	},
+
+	{
+		equippable: true,
+		imageURL: '',
+		isAttuned: false,
+		isEquipped: false,
+		name: 'Plate +1',
+		quantity: 1,
+		rarity: 2,
+		requiresAttunement: false,
+		weight: 65,
+	},
+
+	{
+		equippable: false,
+		imageURL: '',
+		isAttuned: false,
+		isEquipped: false,
+		name: 'Potion of Healing (Greater)',
+		quantity: 5,
+		rarity: 1,
+		requiresAttunement: false,
+		weight: 2.5,
+	},
+
+	{
+		equippable: true,
+		imageURL: '',
+		isAttuned: true,
+		isEquipped: true,
+		name: 'Slippers of Spider Climbing',
+		quantity: 1,
+		rarity: 1,
+		requiresAttunement: true,
+		weight: 0.5,
+	},
+
+	{
+		equippable: false,
+		imageURL: '',
+		isAttuned: false,
+		isEquipped: false,
+		name: 'Backpack',
+		quantity: 1,
+		rarity: 0,
+		requiresAttunement: false,
+		weight: 5,
+	},
+]
 
 
 
@@ -69,16 +162,14 @@ function mapItem(item) {
 }
 
 function CharacterInventory(props) {
-	const { inventory } = props.character
+	const { characterID } = props
+	const { characters } = useCharacters()
+	// const { inventory } = characters[characterID]
 
 	return (
-		<>
-			<h3 className="title">{'Inventory'}</h3>
-
-			<ul className="columns is-multiline">
-				{inventory.map(mapItem)}
-			</ul>
-		</>
+		<ul className="columns is-multiline">
+			{TEMP_INVENTORY.map(mapItem)}
+		</ul>
 	)
 }
 
